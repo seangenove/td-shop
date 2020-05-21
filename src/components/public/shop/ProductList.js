4b1a0908b22f704fb444840421b21bba38f5b073
seanyboyswag <2150218@slu.edu.ph>
2020-05-21 19:08:40 +0800

    Shop component cleanup

---
+++ src/components/public/shop/ProductList.js
@@ -0,0 +1,113 @@
+import React from 'react';
+
+const ProductList = () => {
+
+    const imgStyle = {
+        objectFit: 'cover',
+        height: '359px',
+        overflow: "hidden",
+        objectPosition: "50% -0%"
+    }
+
+    return (
+        <div className="col-sm-6 col-md-8 col-lg-9 p-b-50">
+            <div className="flex-sb-m flex-w p-b-35 d-flex justify-content-end">
+                <span className="s-text8 p-t-5 p-b-5">
+                    Showing 1–12 of 16 results
+                    </span>
+            </div>
+
+            <div className="row">
+                <div className="col-sm-12 col-md-6 col-lg-4 p-b-50">
+                    <div className="block2">
+                        <div className="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+                            <img src="photos/DXDSHirt_IG_left.jpg" alt="IMG-PRODUCT" style={imgStyle} />
+                            <div className="block2-overlay trans-0-4">
+                                <a href="#" className="block2-btn-addwishlist hov-pointer trans-0-4">
+                                    <i className="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+                                    <i className="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+                                </a>
+                                <div className="block2-btn-addcart w-size1 trans-0-4">
+
+                                    <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+                                        Add to Cart
+									</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="block2-txt p-t-20">
+                            <a href="product-detail.html" className="block2-name dis-block s-text3 p-b-5">
+                                DXD Collab Shirt
+                            </a>
+
+                            <span className="block2-price m-text6 p-r-5">
+                                ₱ 900.00
+									</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-4 p-b-50">
+                    <div className="block2">
+                        <div className="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+                            <img src="photos/20200215_184356_328.jpg" alt="IMG-PRODUCT" style={imgStyle} />
+                            <div className="block2-overlay trans-0-4">
+                                <a href="#" className="block2-btn-addwishlist hov-pointer trans-0-4">
+                                    <i className="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+                                    <i className="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+                                </a>
+                                <div className="block2-btn-addcart w-size1 trans-0-4">
+
+                                    <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+                                        Add to Cart
+											</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="block2-txt p-t-20">
+                            <a href="product-detail.html" className="block2-name dis-block s-text3 p-b-5">
+                                Trggrd Hoodie
+									</a>
+
+                            <span className="block2-price m-text6 p-r-5">
+                                ₱ 750.00
+							</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-4 p-b-50">
+                    <div className="block2">
+                        <div className="block2-img wrap-pic-w of-hidden pos-relative block2-labelnew">
+                            <img src="photos/s.png" alt="IMG-PRODUCT" style={imgStyle} />
+                            <div className="block2-overlay trans-0-4">
+                                <a href="#" className="block2-btn-addwishlist hov-pointer trans-0-4">
+                                    <i className="icon-wishlist icon_heart_alt" aria-hidden="true"></i>
+                                    <i className="icon-wishlist icon_heart dis-none" aria-hidden="true"></i>
+                                </a>
+                                <div className="block2-btn-addcart w-size1 trans-0-4">
+
+                                    <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+                                        Add to Cart
+											</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="block2-txt p-t-20">
+                            <a href="product-detail.html" className="block2-name dis-block s-text3 p-b-5">
+                                TRGGRD Statement shirt
+									</a>
+
+                            <span className="block2-price m-text6 p-r-5">
+                                ₱ 75.00
+									</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductList;
--- src/components/public/shop/Shop.js
+++ src/components/public/shop/Shop.js
@@ -1,7 +1,98 @@
 import React from 'react';
 
-const Shop = () => (
-    <h1>Shop</h1>
-)
+import ProductList from './ProductList';
+
+const Shop = () => {
+    return (
+        <div>
+            <section
+                className="bg-title-page p-t-50 p-b-40 flex-col-c-m"
+                style={{ backgroundImage: `url('https://cdn.shopify.com/s/files/1/2672/5778/t/6/assets/collection_top.jpg?v=8714313585269873512')`, minHeight: '150px' }}
+            >
+                <h2 className="l-text2 t-center">PRODUCTS</h2>
+            </section>
+
+            <section className="bgwhite p-t-25 p-b-65">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-6 col-md-4 col-lg-3 p-b-50">
+                            <div className="leftbar p-r-20 p-r-0-sm">
+
+                                <Categories />
+                                <Filters />
+
+                            </div>
+                        </div>
+
+                        <ProductList />
+                        
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+};
+
+const Categories = () => (
+    <div>
+        <h4 className="m-text14 p-b-7">Categories</h4>
+        <ul className="p-b-30">
+            <li className="p-t-4">
+                <a href="#" className="s-text13 active1">All</a>
+            </li>
+            <li className="p-t-4">
+                <a href="#" className="s-text13">Shirts</a>
+            </li>
+            <li className="p-t-4">
+                <a href="#" className="s-text13">Hoodies</a>
+            </li>
+            <li className="p-t-4">
+                <a href="#" className="s-text13">Overalls</a>
+            </li>
+            <li className="p-t-4">
+                <a href="#" className="s-text13">Velvet Suits</a>
+            </li>
+        </ul>
+    </div>
+);
+
+const Filters = () => (
+    <div>
+        <h4 class="m-text14 p-b-32">Filters</h4>
+        <div class="filter-color p-t-22 p-b-50 bo3">
+            <div class="m-text15 p-b-12">Color</div>
+            <ul class="flex-w">
+                <li class="m-r-10">
+                    <input class="checkbox-color-filter" id="color-filter1" type="checkbox" name="color-filter1" />
+                    <label class="color-filter color-filter1" for="color-filter1"></label>
+                </li>
+                <li class="m-r-10">
+                    <input class="checkbox-color-filter" id="color-filter2" type="checkbox" name="color-filter2" />
+                    <label class="color-filter color-filter2" for="color-filter2"></label>
+                </li>
+                <li class="m-r-10">
+                    <input class="checkbox-color-filter" id="color-filter3" type="checkbox" name="color-filter3" />
+                    <label class="color-filter color-filter3" for="color-filter3"></label>
+                </li>
+                <li class="m-r-10">
+                    <input class="checkbox-color-filter" id="color-filter4" type="checkbox" name="color-filter4" />
+                    <label class="color-filter color-filter4" for="color-filter4"></label>
+                </li>
+                <li class="m-r-10">
+                    <input class="checkbox-color-filter" id="color-filter5" type="checkbox" name="color-filter5" />
+                    <label class="color-filter color-filter5" for="color-filter5"></label>
+                </li>
+                <li class="m-r-10">
+                    <input class="checkbox-color-filter" id="color-filter6" type="checkbox" name="color-filter6" />
+                    <label class="color-filter color-filter6" for="color-filter6"></label>
+                </li>
+                <li class="m-r-10">
+                    <input class="checkbox-color-filter" id="color-filter7" type="checkbox" name="color-filter7" />
+                    <label class="color-filter color-filter7" for="color-filter7"></label>
+                </li>
+            </ul>
+        </div>
+    </div>
+);
 
 export default Shop;
