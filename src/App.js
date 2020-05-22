4741d1341fd93c4dc49ab56ac651056719acdb19
seanyboyswag <2150218@slu.edu.ph>
2020-05-22 17:31:05 +0800

    Setup proxy for API server

--- src/App.js
+++ src/App.js
@@ -5,24 +5,7 @@ import PublicApp from './components/layouts/public/PublicApp';
 
 function App() {
   return (
-    //   <h1>test</h1>
       <PublicApp />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
