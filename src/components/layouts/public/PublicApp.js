3f04831a0a2eb0a3e9bf2123f9435889f91cc6e9
seanyboyswag <2150218@slu.edu.ph>
2020-05-23 18:59:39 +0800

    Adjusted implementation of styles for PublicApp

--- src/components/layouts/public/PublicApp.js
+++ src/components/layouts/public/PublicApp.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import '../../../styles/fashe/css/bootstrap.min.css'
+import '../../../styles/fashe/css/main.min.css'
+import '../../../styles/fashe/css/util.min.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import PublicNavBar from './PublicNavBar';
 import PublicFooter from './PublicFooter';
